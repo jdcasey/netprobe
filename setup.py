363e92ae35c23cbe0a834c10a0ab47c03017ec6d
John Casey <jdcasey@commonjava.org>
2020-12-21 16:56:59 -0600

    update to next dev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='netprobe',
-    version='1.0.0',
+    version='1.0.1',
     packages=[''],
     url='https://github.com/jdcasey/netprobe',
     license='GPLv3',
