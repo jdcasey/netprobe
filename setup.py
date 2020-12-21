43d0686a249b7c972769062b811f9bd69a521b7a
John Casey <jdcasey@commonjava.org>
2020-12-21 16:56:23 -0600

    tag with v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='netprobe',
-    version='0.1.0',
+    version='1.0.0',
     packages=[''],
     url='https://github.com/jdcasey/netprobe',
     license='GPLv3',
